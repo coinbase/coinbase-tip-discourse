863c4ad880097acb0e41fcd5b8d8b23ab87f152e
Josh Sorchik <jsorchik@gmail.com>
2014-12-16 10:19:12 +1100

    use a field settable by the user for their cb name

--- controllers/coinbase_controller.rb
+++ controllers/coinbase_controller.rb
@@ -2,10 +2,12 @@ class ::CoinbaseController < ::ApplicationController
 
   def get_tip_id
     u = User.find_by(id: params[:id])
+    f = UserField.find_by(name: "Coinbase Username")
+
     cb_id = 'coinbase'
 
-    if u.custom_fields["coinbase_username"] != nil
-      cb_id = u.custom_fields["coinbase_username"]
+    if u.user_fields["#{f.id}"] != nil
+      cb_id = u.user_fields["#{f.id}"]
     elsif u.single_sign_on_record != nil
       cb_id = u.single_sign_on_record["external_id"]
     end
